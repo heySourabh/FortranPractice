730bdb53a2f9e7b57ecad67e192af2e5ca0cb2fd
Sourabh Bhat <heySourabh@gmail.com>
2020-12-01 21:50:14 +0530

    Simple hello world program

---
+++ fortrantut.f90
@@ -0,0 +1,9 @@
+program fortrantut
+    implicit none
+    character (len = 20) :: f_name, l_name
+
+    print *, "What's your name "
+    read *, f_name, l_name
+    print *, "Hello ", trim(f_name), " ", trim(l_name)
+    
+end program fortrantut
