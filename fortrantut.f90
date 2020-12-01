abc397d8d6b77f59a78749aa26055ae02f291a33
Sourabh Bhat <heySourabh@gmail.com>
2020-12-01 22:26:33 +0530

    left adjust for printing

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,29 +1,8 @@
 program fortrantut
     implicit none
 
-    ! Formatting int: RiW
-
-    ! Formattig float: RfW.D
-    ! R: number of floats
-    ! f: float
-    ! W: width
-    ! D: Decimal places
-
-    print "(2f8.5)", 3.1415, 1.234
-
-    ! Formatting characters: RaW
-    ! /: for new line
-    ! R: number of strings
-    ! a: characters
-    ! W: width
-
-    print "(/, 2a8)", "Name", "Age"
-
-    ! Formatting exponential: e10.3
-
-    print "(e10.3)", 3.1415
-
-    ! Combinig formats:
-    print "(a5, i2)", "I am ", 43
+    character(len=5) :: i_char
+    write (i_char, "(i5)") 10
+    print "(a,a)", "A number ", adjustl(i_char)
 
 end program fortrantut
