c6138ef03b828989b8e9ba1734b03b1e9081cba8
Sourabh Bhat <heySourabh@gmail.com>
2020-12-01 22:08:12 +0530

    Some formatting for integers

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,19 +1,14 @@
 program fortrantut
     implicit none
-    real, parameter :: PI = 3.1415
-    real :: r_num1 = 0.0, r_num2 = 0.0
-    double precision :: dbl_num = 1.111111111111111111d+0
-    integer :: i_num1 = 0, i_num2 = 0
-    logical :: can_vote = .true.
-    character (len = 10) :: month
-    complex :: com_num = (2.0, 4.0)
     
-    print *, "Biggest Real ", huge(r_num1)
-    print *, "Biggest Int ", huge(i_num1)
-    print *, "Smallest Real ", tiny(r_num1)
-    print "(a4, i1)", "Int ", kind(i_num1)
-    print "(a5, i1)", "Real ", kind(r_num1)
-    print "(a7, i1)", "Double ", kind(dbl_num)
-    print "(a8, i1)", "Logical ", kind(can_vote)
+    print *, "A number ", 10
+    print "(3i5)", 7, 6, 8
+    print "(i5)", 7, 6, 8
+! Outputs: 
+! A number           10
+!    7    6    8
+!    7
+!    6
+!    8
 
 end program fortrantut
