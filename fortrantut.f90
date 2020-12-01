8e281608d43be7d425752042ff032113ab94e848
Sourabh Bhat <heySourabh@gmail.com>
2020-12-01 22:21:09 +0530

    Some formatting with print

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,14 +1,29 @@
 program fortrantut
     implicit none
-    
-    print *, "A number ", 10
-    print "(3i5)", 7, 6, 8
-    print "(i5)", 7, 6, 8
-! Outputs: 
-! A number           10
-!    7    6    8
-!    7
-!    6
-!    8
+
+    ! Formatting int: RiW
+
+    ! Formattig float: RfW.D
+    ! R: number of floats
+    ! f: float
+    ! W: width
+    ! D: Decimal places
+
+    print "(2f8.5)", 3.1415, 1.234
+
+    ! Formatting characters: RaW
+    ! /: for new line
+    ! R: number of strings
+    ! a: characters
+    ! W: width
+
+    print "(/, 2a8)", "Name", "Age"
+
+    ! Formatting exponential: e10.3
+
+    print "(e10.3)", 3.1415
+
+    ! Combinig formats:
+    print "(a5, i2)", "I am ", 43
 
 end program fortrantut
