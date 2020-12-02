b5f68ddde27543a0b2630c98a6d800a52ce44cda
Sourabh Bhat <heySourabh@gmail.com>
2020-12-02 14:15:59 +0530

    Math functions

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,17 +1,21 @@
 program fortrantut
     implicit none
 
-    real :: float_num = 1.11111111111111
-    real :: float_num2 = 1.11111111111111
-    double precision :: dbl_num = 1.11111111111111111d+0
-    double precision :: dbl_num2 = 1.11111111111111111d+0
-    real :: rand(1)
-    integer :: low = 1, high = 10
+    print "(a10, i1)"  , "ABS(-1) = ", abs(-1)
+    print "(a11, f3.1)", "SQRT(81) = ", sqrt(81.0)
+    print "(a9, f7.5)" , "EXP(1) = ", exp(1.0)
+    print "(a12, f7.5)", "LOG(2.71) = ", log(2.71)
+    print "(a12, i1)"  , "INT(2.71) = ", int(2.71)
+    print "(a13, i1)"  , "NINT(2.71) = ", nint(2.71)
+    print "(a14, i1)"  , "FLOOR(2.71) = ", floor(2.71)
+    print "(a15, f3.1)", "MAX(2.7, 3.4) = ", max(2.7, 3.4)
+    print "(a15, f3.1)", "MIN(2.7, 3.4) = ", min(2.7, 3.4)
+    print "(a14, f3.1)", "SIN(1.5708) = ", sin(1.5708)
+    print "(a14, f3.1)", "COS(1.5708) = ", cos(1.5708)
+    print "(a14, f10.1)", "TAN(1.5708) = ", tan(1.5708)
+    print "(a10, f3.1)", "ASIN(0) = ", asin(0.0)
+    print "(a10, f3.1)", "ACOS(0) = ", acos(0.0)
+    print "(a10, f3.1)", "ATAN(0) = ", atan(0.0)
 
-    print "(f17.15)", float_num + float_num2
-    print "(f17.15)", dbl_num + dbl_num2
-
-    call random_number(rand)
-    print "(i2)", low + floor((high + 1 - low) * rand)
 
 end program fortrantut
