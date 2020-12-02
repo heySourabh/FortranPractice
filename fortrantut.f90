2d75396e9f4b999f65cffe9a9ba13a570e8b8e31
Sourabh Bhat <heySourabh@gmail.com>
2020-12-02 22:13:45 +0530

    do while loop

--- fortrantut.f90
+++ fortrantut.f90
@@ -2,10 +2,18 @@ program fortrantut
     implicit none
 
     integer :: n = 0, m = 1
-    integer :: secret_num = 7
 
-    do n = 1, 10, 2
-        print "(i1)", n
+    do while (m < 20)
+        if(mod(m, 2) == 0) then 
+            print "(i1)", m
+            m = m + 1
+            cycle  !same as continue
+        end if
+        m = m + 1
+        if(m >= 10) then
+            exit   !same as break loop
+        end if
     end do
+
 
 end program fortrantut
