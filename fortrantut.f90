262e1f33afed5dd935dc295466cbb228daeb920a
Sourabh Bhat <heySourabh@gmail.com>
2020-12-02 14:25:13 +0530

    logical operators

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,21 +1,20 @@
 program fortrantut
     implicit none
 
-    print "(a10, i1)"  , "ABS(-1) = ", abs(-1)
-    print "(a11, f3.1)", "SQRT(81) = ", sqrt(81.0)
-    print "(a9, f7.5)" , "EXP(1) = ", exp(1.0)
-    print "(a12, f7.5)", "LOG(2.71) = ", log(2.71)
-    print "(a12, i1)"  , "INT(2.71) = ", int(2.71)
-    print "(a13, i1)"  , "NINT(2.71) = ", nint(2.71)
-    print "(a14, i1)"  , "FLOOR(2.71) = ", floor(2.71)
-    print "(a15, f3.1)", "MAX(2.7, 3.4) = ", max(2.7, 3.4)
-    print "(a15, f3.1)", "MIN(2.7, 3.4) = ", min(2.7, 3.4)
-    print "(a14, f3.1)", "SIN(1.5708) = ", sin(1.5708)
-    print "(a14, f3.1)", "COS(1.5708) = ", cos(1.5708)
-    print "(a14, f10.1)", "TAN(1.5708) = ", tan(1.5708)
-    print "(a10, f3.1)", "ASIN(0) = ", asin(0.0)
-    print "(a10, f3.1)", "ACOS(0) = ", acos(0.0)
-    print "(a10, f3.1)", "ATAN(0) = ", atan(0.0)
+    integer :: age = 16
+    if((age >= 5) .and. (age <= 6)) then
+        print *, "Kindergarden"
+    else if ((age >= 7) .and. (age <= 13)) then
+        print *, "Middle school"
+    else if ((age >= 14) .and. (age <= 18)) then
+        print *, "High school"
+    else 
+        print *, "Stay home"
+    end if
 
+    print *, .true. .or. .false.
+    print *, .not. .true.
+    print *, 5 /= 9
+    print *, "a" < "b"
 
 end program fortrantut
