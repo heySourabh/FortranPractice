b6d5f915cd9fbeb634f6cf896a7c6d7b79f8f3c1
Sourabh Bhat <heySourabh@gmail.com>
2020-12-02 22:19:13 +0530

    do while loop example

--- fortrantut.f90
+++ fortrantut.f90
@@ -2,18 +2,13 @@ program fortrantut
     implicit none
 
     integer :: n = 0, m = 1
+    integer :: secret_num = 7
 
-    do while (m < 20)
-        if(mod(m, 2) == 0) then 
-            print "(i1)", m
-            m = m + 1
-            cycle  !same as continue
-        end if
-        m = m + 1
-        if(m >= 10) then
-            exit   !same as break loop
-        end if
+    do while (n /= secret_num)   ! /= means not equal to 
+        print *, "What's your guess"
+        read *, n
     end do
+    print *, "You guessed it"
 
 
 end program fortrantut
