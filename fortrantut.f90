1c29fe63d2753e469d8537c77d8061b18429fa9d
Sourabh Bhat <heySourabh@gmail.com>
2020-12-02 13:46:13 +0530

    Simple math operation

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,8 +1,18 @@
 program fortrantut
     implicit none
 
-    character(len=5) :: i_char
-    write (i_char, "(i5)") 10
-    print "(a,a)", "A number ", adjustl(i_char)
+    real :: float_num = 1.11111111111111
+    real :: float_num2 = 1.11111111111111
+    double precision :: dbl_num = 1.11111111111111111d+0
+    double precision :: dbl_num2 = 1.11111111111111111d+0
+    real :: rand(1)
+    integer :: low = 1, high = 10
+
+    print "(a8, i1)", "5 + 4 = ", (5 + 4)
+    print "(a8, i1)", "5 - 4 = ", (5 - 4)
+    print "(a8, i2)", "5 * 4 = ", (5 * 4)
+    print "(a8, i1)", "5 / 4 = ", (5 / 4)
+    print "(a8, i1)", "5 % 4 = ", mod(5, 4)
+    print "(a8, i3)", "5**4 = ", (5**4)
 
 end program fortrantut
