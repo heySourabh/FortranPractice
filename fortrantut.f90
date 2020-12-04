03c69c490aeecf756ad4f2f0ac1e231d879d213f
Sourabh Bhat <heySourabh@gmail.com>
2020-12-04 19:44:36 +0530

    Some operations with arrays

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,14 +1,53 @@
 program fortrantut
     implicit none
 
-    integer :: n = 0, m = 1
-    integer :: secret_num = 7
+    integer, dimension(1:5) :: a1, a2, a3
+    real, dimension(1:50) :: aR1
+    integer, dimension(5,5) :: a4
+    integer :: n, m, x, y
+    integer, dimension(:), allocatable :: a5
+    integer :: num_vals = 0
+    integer, dimension(1:9) :: a6 = (/ 1, 2, 3, 4, 5, 6, 7, 8, 9 /)
+    integer, dimension(1:3, 1:3) :: a7
 
-    do while (n /= secret_num)   ! /= means not equal to 
-        print *, "What's your guess"
-        read *, n
+    a1(1) = 5
+    print "(i1)", a1(1)
+
+    do n = 1, 5
+        a1(n) = n
     end do
-    print *, "You guessed it"
 
+    do n = 1, 5
+        print "(i1)", a1(n)
+    end do
+
+    print "(3i2)", a1(1:3)
+    print "(2i2)", a1(1:3:2)
+
+    do n = 1,5
+        do m = 1, 5
+            a4(n,m) = n
+        end do
+    end do
+
+    do n = 1,5
+        do m = 1, 5
+            print "(i1, a1, i1, a3, i1)", n, " ", m, " : ", a4(n, m)
+        end do
+    end do
+
+    ! Implied do loop
+    print *
+    print *, "Implied do loop: "
+
+    do n = 1, 5
+        print "(5i1)", ( a4(n, m), m = 1,5)
+    end do
+
+    print "(a11, i2)", "Size: ", size(a1)
+    print "(a11, i2)", "Size(5x5): ", size(a4)
+
+    print "(a11, i2)", "Rank: ", rank(a4)
+    print "(a11, i2)", "Shape: ", shape(a4)
 
 end program fortrantut
