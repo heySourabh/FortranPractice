3e1c6a3364700f14aa3ed68044ff5081c983a173
Sourabh Bhat <heySourabh@gmail.com>
2020-12-12 22:18:17 +0530

    formattimg

--- fortrantut.f90
+++ fortrantut.f90
@@ -1,79 +1,20 @@
 program fortrantut
     implicit none
 
-    integer, dimension(1:5) :: a1, a2, a3
-    real, dimension(1:50) :: aR1
-    integer, dimension(5,5) :: a4
-    integer :: n, m, x, y
-    integer, dimension(:), allocatable :: a5
-    integer :: num_vals = 0
-    integer, dimension(1:9) :: a6 = (/ 1, 2, 3, 4, 5, 6, 7, 8, 9 /)
-    integer, dimension(1:3, 1:3) :: a7
+    integer :: num, cups
+    real :: liters, quarts
 
-    a1(1) = 5
-    print "(i1)", a1(1)
-
-    do n = 1, 5
-        a1(n) = n
+    do num = 1, 12
+        print 100, num, num * 7
+        100 format(i2, " * 7 = ", i3)
     end do
 
-    do n = 1, 5
-        print "(i1)", a1(n)
+    print "(/a18)", "Cups Liters Quarts"
+    do cups = 1, 10
+        liters = cups * 0.236
+        quarts = cups * 0.208
+        print 200, cups, liters, quarts
+        200 format(" ", i3, 2x, f5.3, 2x, f5.3)
     end do
-
-    print "(3i2)", a1(1:3)
-    print "(2i2)", a1(1:3:2)
-
-    do n = 1,5
-        do m = 1, 5
-            a4(n,m) = n
-        end do
-    end do
-
-    do n = 1,5
-        do m = 1, 5
-            print "(i1, a1, i1, a3, i1)", n, " ", m, " : ", a4(n, m)
-        end do
-    end do
-
-    ! Implied do loop
-    print *
-    print *, "Implied do loop: "
-
-    do n = 1, 5
-        print "(5i1)", ( a4(n, m), m = 1,5)
-    end do
-
-    print "(a11, i2)", "Size: ", size(a1)
-    print "(a11, i2)", "Size(5x5): ", size(a4)
-
-    print "(a11, i2)", "Rank: ", rank(a4)
-    print "(a11, i2)", "Shape: ", shape(a4)
-
-    print *, "Size of array? "
-    read *, num_vals
-    allocate(a5(1:num_vals))
-
-    do n = 1, num_vals
-        a5(n) = n
-    end do
-    do n = 1, num_vals
-        print "(i1)", a5(n)
-    end do
-
-    a2 = (/1, 2, 4, 6, 7/)
-
-    print "(10i2)", (a2(m), m = 1,5)   ! implied loop
-
-    a7 = reshape(a6, (/3, 3/))
-    
-    print "(l1)", all(a1 == a2, 1)
-    print "(l1)", any(a1 == a2, 1)
-    print "(l1)", count(a1 == a2, 1)
-    print "(i3)", maxval(a1)
-    print "(i3)", minval(a1)
-
-    print "(i3)", product(a1)
-    print "(i2)", sum(a1)
 
 end program fortrantut
