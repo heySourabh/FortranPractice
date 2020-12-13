1c69e4c93856b873e0c8daa5a785126dab8805cf
Sourabh Bhat <heySourabh@gmail.com>
2020-12-13 17:09:28 +0530

    Subroutine

--- fortrantut.f90
+++ fortrantut.f90
@@ -2,11 +2,24 @@ program fortrantut
     use mult_mod
     implicit none
 
-    real :: r_ans
+    integer :: n
+    integer :: p1 
+    integer :: p2
 
-    print *, "5 * 4 = ", mult(5, 4)
+    n = 1
+    call plus_two(n, p1, p2)
 
-    r_ans = mult(5.1, 4.4)
-    print *, "5.1 * 4.4 = ", r_ans
+    print "(i1, /, i1, /, i1)", n, p1, p2
+
+    contains
+
+    subroutine plus_two(i, plus1, plus2)
+        integer, intent(in) :: i
+        integer, intent(out) :: plus1, plus2
+
+        plus1 = i + 1
+        plus2 = i + 2
+
+    end subroutine plus_two
 
 end program fortrantut
